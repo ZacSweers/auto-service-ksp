2277f4fbe143c2227775eea8f03a0cf8eaee8d1b
Zac Sweers <pandanomic@gmail.com>
2021-04-09 21:51:38 -0400

    Update deps (#12)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,37 +16,37 @@
 
 object Dependencies {
 
-  const val guava = "com.google.guava:guava:30.0-jre"
+  const val guava = "com.google.guava:guava:30.1.1-jre"
 
   object AutoService {
-    private const val version = "1.0-rc7"
+    private const val version = "1.0"
     const val annotations = "com.google.auto.service:auto-service-annotations:$version"
     const val processor = "com.google.auto.service:auto-service:$version"
   }
 
   object Kotlin {
-    const val version = "1.4.30"
-    const val dokkaVersion = "1.4.20"
+    const val version = "1.4.32"
+    const val dokkaVersion = "1.4.30"
     const val jvmTarget = "1.8"
     val defaultFreeCompilerArgs = listOf("-Xjsr305=strict", "-progressive")
     const val compilerEmbeddable = "org.jetbrains.kotlin:kotlin-compiler-embeddable:$version"
 
     object Ksp {
-      private const val version = "1.4.30-1.0.0-alpha04"
+      private const val version = "1.4.32-1.0.0-alpha07"
       const val api = "com.google.devtools.ksp:symbol-processing-api:$version"
       const val ksp = "com.google.devtools.ksp:symbol-processing:$version"
     }
   }
 
   object KotlinPoet {
-    private const val version = "1.7.2"
+    private const val version = "1.8.0"
     const val kotlinPoet = "com.squareup:kotlinpoet:$version"
   }
 
   object Testing {
-    const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.3.4"
-    const val kspCompileTesting = "com.github.tschuchortdev:kotlin-compile-testing-ksp:1.3.4"
-    const val junit = "junit:junit:4.13.1"
-    const val truth = "com.google.truth:truth:1.1"
+    const val compileTesting = "com.github.tschuchortdev:kotlin-compile-testing:1.3.6"
+    const val kspCompileTesting = "com.github.tschuchortdev:kotlin-compile-testing-ksp:1.3.6"
+    const val junit = "junit:junit:4.13.2"
+    const val truth = "com.google.truth:truth:1.1.2"
   }
 }
