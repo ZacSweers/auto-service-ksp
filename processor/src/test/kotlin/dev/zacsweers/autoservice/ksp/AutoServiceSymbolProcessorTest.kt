397347d6933ce55b2e2471ace58ad7494ad14301
YOUNG HO CHA <ganachoco@linecorp.com>
2021-06-27 14:11:09 +0900

    Added unittest for java source

--- processor/src/test/kotlin/dev/zacsweers/autoservice/ksp/AutoServiceSymbolProcessorTest.kt
+++ processor/src/test/kotlin/dev/zacsweers/autoservice/ksp/AutoServiceSymbolProcessorTest.kt
@@ -7,6 +7,7 @@ import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.kspIncremental
 import com.tschuchort.compiletesting.kspSourcesDir
 import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -36,6 +37,35 @@ class AutoServiceSymbolProcessorTest(private val incremental: Boolean) {
       @AutoService(Callable::class)
       class CustomCallable : Callable<String> {
         override fun call(): String = "Hello world!"
+      }
+    """)
+
+    val compilation = KotlinCompilation().apply {
+      sources = listOf(source)
+      inheritClassPath = true
+      symbolProcessorProviders = listOf(AutoServiceSymbolProcessorProvider())
+      kspIncremental = incremental
+    }
+    val result = compilation.compile()
+    assertThat(result.exitCode).isEqualTo(ExitCode.OK)
+    val generatedSourcesDir = compilation.kspSourcesDir
+    val generatedFile = File(generatedSourcesDir,
+      "resources/META-INF/services/java.util.concurrent.Callable")
+    assertThat(generatedFile.exists()).isTrue()
+    assertThat(generatedFile.readText()).isEqualTo("test.CustomCallable\n")
+  }
+
+  @Ignore("https://github.com/tschuchortdev/kotlin-compile-testing/issues/105")
+  @Test
+  fun smokeTestForJava() {
+    val source = SourceFile.java("CustomCallable.java", """
+      package test;
+      import com.google.auto.service.AutoService;
+      import java.util.concurrent.Callable;
+
+      @AutoService(Callable.class)
+      public class CustomCallable implements Callable<String> {
+        @Override public String call() { return "Hello world!"; }
       }
     """)
 
